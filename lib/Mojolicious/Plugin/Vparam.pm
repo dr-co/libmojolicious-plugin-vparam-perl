76d2480d63864688ed73174e2e92ed3fd7a81242
Dmitry E. Oboukhov <unera@debian.org>
2017-03-24 17:57:08 +0300

    Remove README.pod before build debian package.

--- lib/Mojolicious/Plugin/Vparam.pm
+++ lib/Mojolicious/Plugin/Vparam.pm
@@ -5,7 +5,7 @@ use Mojolicious::Plugin::Vparam::Common qw(:all);
 use version;
 use List::MoreUtils qw(firstval natatime mesh);
 
-our $VERSION    = '2.05';
+our $VERSION    = '2.06';
 
 # Regext for shortcut parser
 our $SHORTCUT_REGEXP = qr{
